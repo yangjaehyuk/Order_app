9368d3651a3aa1bf1205662aca39d0348b93fc71
yangjaehyuk <gndksakxk10@naver.com>
2023-10-12 16:47:22 +0900

    feat: 옵션 정보 가져오기

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Options = () => {
-  return <div>Options</div>;
+const Options = ({ name }) => {
+  return (
+    <form>
+      <input type="checkbox" id={`${name} option`} />{' '}
+      <label htmlFor={`${name} option`}>{name}</label>
+    </form>
+  );
 };
 
 export default Options;
